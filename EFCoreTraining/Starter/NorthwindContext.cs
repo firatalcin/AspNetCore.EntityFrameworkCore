4cac2353549c2a8fe2365b010dc9e4e410854b26
Fırat Alçın <firatalcin04@gmail.com>
2024-04-07 15:15:29 +0300

    Ders 1 - Neden ORM Kullanmalıyız ?

---
+++ EFCoreTraining/Starter/NorthwindContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Starter.Model;
+
+namespace Starter
+{
+    public class NorthwindContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            ConfigurationManager configuration = new();
+            configuration.AddJsonFile("appsettings.json");
+            optionsBuilder.UseSqlServer($"Server=localhost, 1433;Database=Northwind;User Id=sa;Password={configuration["Password"]}");
+        }
+    }
+}
