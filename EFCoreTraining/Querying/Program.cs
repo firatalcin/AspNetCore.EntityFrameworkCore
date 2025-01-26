82a28549a19ece51cda2136b52fe28ab37937c54
Fırat Alçın <firatalcin04@gmail.com>
2025-01-26 17:10:27 +0300

    Query 1

---
+++ EFCoreTraining/Querying/Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+#region En Temel Basit Bir Sorgulama Nasıl Yapılır?
+#region Method Syntax
+//var urunler = await context.Urunler.ToListAsync();
+#endregion
+#region Query Syntax
+//var urunler2 = await (from urun in context.Urunler
+//                      select urun).ToListAsync();
+#endregion
+#endregion
+
+#region Sorguyu Execute Etmek İçin Ne Yapmamız Gerekmektedir?
+#region ToListAsync 
+#region Method Syntax
+//var urunler = await context.Urunler.ToListAsync();
+#endregion
+#region Query Syntax
+//var urunler = await (from urun in context.Urunler
+//select urun).ToListAsync();
+#endregion
+#endregion
+
+//int urunId = 5;
+//string urunAdi = "2";
+
+//var urunler = from urun in context.Urunler
+//              where urun.Id > urunId && urun.UrunAdi.Contains(urunAdi)
+//              select urun;
+
+//urunId = 200;
+//urunAdi = "4";
+
+//foreach (Urun urun in urunler)
+//{
+//    Console.WriteLine(urun.UrunAdi);
+//}
+
+//await urunler.ToListAsync();
+
+#region Foreach
+
+//foreach (Urun urun in urunler)
+//{
+//    Console.WriteLine(urun.UrunAdi);
+//}
+
+#region Deferred Execution(Ertelenmiş Çalışma)
+//IQueryable çalışmalarında ilgili kod yazıldığı noktada tetiklenmez/çalıştırılmaz yani ilgili kod yazıldığı noktada sorguyu generate etmez! Nerede eder? Çalıştırıldığı/execute edildiği noktada tetiklenir! İşte bu durumada ertelenmiş çalışma denir!
+#endregion
+#endregion
+#endregion
+
+#region IQueryable ve IEnumerable Nedir? Basit Olarak!
