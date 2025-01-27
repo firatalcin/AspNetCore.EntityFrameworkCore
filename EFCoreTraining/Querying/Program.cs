031fdb4d8d61a70286512735d4ece3bd933b3b0c
Fırat Alçın <firatalcin04@gmail.com>
2025-01-27 22:50:00 +0300

    Query 2

--- EFCoreTraining/Querying/Program.cs
+++ EFCoreTraining/Querying/Program.cs
@@ -52,3 +52,83 @@
 #endregion
 
 #region IQueryable ve IEnumerable Nedir? Basit Olarak!
+
+#region Çoğul Veri Getiren Sorgulama Fonksiyonları
+#region ToListAsync
+//Üretilen sorguyu execute ettirmemizi sağlayan fonksiyondur.
+
+#region Method Syntax
+//var urunler = context.Urunler.ToListAsync();
+#endregion
+#region Query Syntax
+//var urunler = (from urun in context.Urunler
+//              select urun).ToListAsync();
+//var urunler = from urun in context.Urunler
+//              select urun;
+//var datas = await urunler.ToListAsync();
+#endregion
+#endregion
+
+#region Where
+//Oluşturulan sorguya where şartı eklememizi sağlayan bir fonksiyondur.
+
+#region Method Syntax
+//var urunler = await context.Urunler.Where(u => u.Id > 500).ToListAsync();
+//var urunler = await context.Urunler.Where(u => u.UrunAdi.StartsWith("a")).ToListAsync();
+//Console.WriteLine();
+#endregion
+#region Query Syntax
+//var urunler = from urun in context.Urunler
+//              where urun.Id > 500 && urun.UrunAdi.EndsWith("7")
+//              select urun;
+//var data = await urunler.ToListAsync();
+//Console.WriteLine();
+#endregion
+#endregion
+
+#region OrderBy
+//Sorgu üzerinde sıralama yapmamızı sağlayan bir fonksiyondur. (Ascending)
+
+#region Method Syntax
+//var urunler = context.Urunler.Where(u => u.Id > 500 || u.UrunAdi.EndsWith("2")).OrderBy(u => u.UrunAdi);
+#endregion
+#region Query Syntax
+//var urunler2 = from urun in context.Urunler
+//               where urun.Id > 500 || urun.UrunAdi.StartsWith("2")
+//               orderby urun.UrunAdi
+//               select urun;
+#endregion
+
+//await urunler.ToListAsync();
+//await urunler2.ToListAsync();
+#endregion
+
+#region ThenBy
+//OrderBy üzerinde yapılan sıralama işlemini farklı kolonlarada uygulamamızı sağlayan bir fonksiyondur. (Ascending) 
+
+//var urunler = context.Urunler.Where(u => u.Id > 500 || u.UrunAdi.EndsWith("2")).OrderBy(u => u.UrunAdi).ThenBy(u => u.Fiyat).ThenBy(u => u.Id);
+
+//await urunler.ToListAsync();
+#endregion
+
+#region OrderByDescending
+//Descending olarak sıralama yapmamızı sağlayan bir fonksiyondur.
+
+#region Method Syntax
+//var urunler = await context.Urunler.OrderByDescending(u => u.Fiyat).ToListAsync();
+#endregion
+#region Query Syntax
+//var urunler = await (from urun in context.Urunler
+//                     orderby urun.UrunAdi descending
+//                     select urun).ToListAsync();
+#endregion
+#endregion
+
+#region ThenByDescending
+//OrderByDescending üzerinde yapılan sıralama işlemini farklı kolonlarada uygulamamızı sağlayan bir fonksiyondur. (Ascending)
+//var urunler = await context.Urunler.OrderByDescending(u => u.Id).ThenByDescending(u => u.Fiyat).ThenBy(u => u.UrunAdi).ToListAsync();
+#endregion
+
+#endregion
+
+Console.ReadLine();
